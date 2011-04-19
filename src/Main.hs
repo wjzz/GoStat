b75d8def0056c913ec58c6b5cc5f61a55e33c971
Wojtek <wjedynak@gmail.com>
2011-04-19 22:48:55 +0200

    Basic symmetry bug is fixed, a major one is discovered ;)

--- src/Main.hs
+++ src/Main.hs
@@ -22,7 +22,5 @@ main = do
   -- addFilesToDB
   -- putStrLn "DB rebuilding done!"
   server  
-  
---loadSGFs >>= \l -> print (sortBy (compare `on` snd) $ map (head &&& length) $ group $ sort $ map (head . moves) l)
-  --loadSGFs >>= \l -> print (length l)
+
 
