84e1d5a7e6d6041bf1fc7f1c57573352c1d51120
Wojtek <wjedynak@gmail.com>
2011-05-07 21:11:53 +0200

    Parallelized generation; Haven't broke a sweat - Thanks Haskell\!

--- src/Offline.hs
+++ src/Offline.hs
@@ -4,6 +4,8 @@
 module Offline where
 
 import Control.Concurrent
+import Control.Exception
+import System.IO.Unsafe
 
 import Pages
 import DB
@@ -18,7 +20,8 @@ buildOffline :: Level -> IO ()
 buildOffline level = do 
   putStrLn "Building offline version..."
   saveMainPage
-  buildMoveBrowser level ""
+  buildMoveBrowser level 0 ""
+  waitForChildren
 
 saveMainPage :: IO ()
 saveMainPage = do
@@ -26,14 +29,17 @@ saveMainPage = do
   let html = mainPage offLineConfig count
   writeFile (path ++ "index.htm") (renderHtml html)
   
-buildMoveBrowser :: Level -> String -> IO ()
-buildMoveBrowser 0 _ = return ()
-buildMoveBrowser n movesSoFar = do
+buildMoveBrowser :: Level -> Level -> String -> IO ()
+buildMoveBrowser 0 _ _ = return ()
+buildMoveBrowser n currentLevel movesSoFar = do
   moves <- queryStatsDB movesSoFar
   let html = moveBrowser moves movesSoFar offLineConfig
   writeFile (path ++ "moves/" ++ (mbUrl movesSoFar)) (renderHtml html) 
   let options = map (\(m,_,_,_) -> m) moves
-  mapM_ (\move -> buildMoveBrowser (n-1) (movesSoFar ++ move)) options
+  mapM_ (\move -> aux $ buildMoveBrowser (n-1) (currentLevel+1) (movesSoFar ++ move)) options where
+    aux m
+      | currentLevel < 3 = forkChild m >> return ()
+      | otherwise        = m
 
 offLineConfig :: Configuration
 offLineConfig = Configuration { mainPageUrl        = "../index.htm"
@@ -48,3 +54,26 @@ mbUrl movesSoFar = "move" ++ movesSoFar ++ ".htm"
 
 imageUrl :: String -> String
 imageUrl image = "../img/" ++ image
+
+children :: MVar [MVar ()]
+children = unsafePerformIO (newMVar [])
+    
+waitForChildren :: IO ()
+waitForChildren = do
+  cs <- takeMVar children
+  case cs of
+    []   -> return ()
+    m:ms -> do
+      putMVar children ms
+      takeMVar m
+      waitForChildren
+
+forkChild :: IO () -> IO ThreadId
+forkChild io = do
+  mvar <- newEmptyMVar
+  childs <- takeMVar children
+  putMVar children (mvar:childs)
+  forkIO (io `finally` putMVar mvar ())
+
+-- main =
+--   later waitForChildren $
